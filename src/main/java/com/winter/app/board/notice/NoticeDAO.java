3735e4af7e5c915eab53b1813c40af5488a6a4e8
otlb <an95621@naver.com>
2024-01-12 17:47:42 +0900

    add

---
+++ src/main/java/com/winter/app/board/notice/NoticeDAO.java
@@ -0,0 +1,46 @@
+package com.winter.app.board.notice;
+
+import java.util.List;
+
+import com.winter.app.board.BoardDAO;
+import com.winter.app.board.BoardDTO;
+
+public class NoticeDAO implements BoardDAO {
+
+	@Override
+	public Long getTotalCount() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardDTO> getList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setAdd(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
